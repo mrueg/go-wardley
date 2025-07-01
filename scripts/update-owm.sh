c89fb2e313b63401906ebcb238009131109c0cb1
Manuel Rüger <manuel@rueg.eu>
2025-07-01 21:42:46 +0200

    Update owm to b6f01b8e14e4974f3cb216e64670dbcb4606c97b

--- scripts/update-owm.sh
+++ scripts/update-owm.sh
@@ -8,6 +8,6 @@ git clone git@github.com:damonsk/onlinewardleymaps.git --depth=1 "${TMP_DIR}"
 
 cp next.config.js "${TMP_DIR}"/frontend 
 
-docker run -it --rm -v "${TMP_DIR}":/tmp -w /tmp/frontend node:18-alpine3.18 sh -c "yarn install && yarn cache clean && yarn build"
+docker run -it --rm -v "${TMP_DIR}":/tmp -w /tmp/frontend node:22-alpine3.21 sh -c "yarn install && yarn cache clean && yarn build"
 
 rsync -avx --delete "${TMP_DIR}"/frontend/dist/ ../wardley/dist/
